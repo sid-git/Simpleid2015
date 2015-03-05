4c7e0b4bdcf7a6ee442c8c0e651d1ccae5a9a561
lili0allen <lili0allen@gmail.com>
2015-03-05 15:01:49 +1100

    use is_tree function to detect pages

--- twentytwelve/header.php
+++ twentytwelve/header.php
@@ -34,7 +34,7 @@
 <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>-->
 <!--<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.1.min.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-ui.js" type="text/javascript"></script>-->
-<?php if((!is_page(array(2602,2604,2606,2609,2612,2614,2616,2618,2620,2622,2526)))&&(!is_home())){ ?>
+<?php  if((!is_tree(2818))&&(!is_home())){/* if it is IT solution page or its children pages or home page */ ?>
 <link href="<?php echo get_template_directory_uri(); ?>/css/simple-notdelete.css" id="oldcss" rel="stylesheet" type="text/css">
 <link href="<?php echo get_template_directory_uri(); ?>/css/reset.css" rel="stylesheet" type="text/css">
 <link href="<?php echo get_template_directory_uri(); ?>/edit.css" rel="stylesheet" type="text/css">
@@ -47,6 +47,8 @@
 <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/92ddbb5b-7060-4daf-af08-167d00a49485.css"/>
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+
 <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -147,7 +149,7 @@ wp_nav_menu( $defaults );
 	    </div><!-- /.navbar-collapse -->
 	  </div><!-- /.container-fluid -->
 	</nav>
-<?php if(is_page(2526)||is_page(2749)||is_page(2638)||is_page(2622)||is_page(2620)||is_page(2618)||is_page(2616)||is_page(2614)||is_page(2612)||is_page(2609)||is_page(2606)||is_page(2604)){ ?>
+<?php if(is_tree(2818)){ //if it is IT solution page or its children pages ?>
 <div class="float-quote">
 <span>
 Get a Free Quote<br/>
